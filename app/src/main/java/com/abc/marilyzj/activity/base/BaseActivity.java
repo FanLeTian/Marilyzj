6c86e248b3d5bd7c53174c3c0940708057f5709e
FanLeTian <837592427@qq.com>
2017-05-08 17:00:34 +0800

    commit

--- app/src/main/java/com/abc/marilyzj/activity/base/BaseActivity.java
+++ app/src/main/java/com/abc/marilyzj/activity/base/BaseActivity.java
@@ -1,6 +1,8 @@
 package com.abc.marilyzj.activity.base;
 
+import android.os.Bundle;
 import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
@@ -20,6 +22,11 @@ public abstract class BaseActivity extends AppCompatActivity {
     protected Toolbar toolbar;
     protected TextView customTitle;
     private LoadingDialog mLoadingDialog;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     @Override
     public void setContentView(@LayoutRes int layoutResID) {
